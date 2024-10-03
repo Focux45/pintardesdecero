63d4bcbb65631e7bd6016e88f5873f1735e4147f
Focux45 <134445296+Focux45@users.noreply.github.com>
2024-10-03 07:13:58 -0500

    Update App.py

--- App.py
+++ App.py
@@ -8,14 +8,14 @@ st.subheader("Dibuja en el panel")
 # Add canvas component
 # Specify canvas parameters in application
 drawing_mode = "freedraw"
-
 #stroke_color = '#00000' # Set background color to white
 bg_color = '#ffffff'
-stroke_color = st.color_picker("Pick A Color", "#00f900")
+
 st.write("The current color is", stroke_color)
 with st.sidebar:
     st.subheader("Propiedades")
     stroke_width = st.slider('Selecciona el ancho de línea', 1, 30, 10)
+    stroke_color = st.color_picker("Pick A Color", "#00f900")
 
 canvas_result = st_canvas(
     fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
