4860472f81732b6149c9f86809dc90b1a3c5af06
Focux45 <134445296+Focux45@users.noreply.github.com>
2024-10-03 06:53:39 -0500

    Update App.py

--- App.py
+++ App.py
@@ -1,9 +1,23 @@
+import streamlit as st
+
+st.set_page_config(page_title='Pintando con estilo', layout='wide')
+st.title('Vamos a dibujar algo pro')
+st.subheader("Dibuja en el panel")
+
+# Add canvas component
+# Specify canvas parameters in application
+drawing_mode = "freedraw"
+stroke_width = st.slider('Selecciona el ancho de línea', 1, 30, 15)
+stroke_color = '#FFFFFF' # Set background color to white
+bg_color = '#000000'
+
+
 canvas_result = st_canvas(
     fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
     stroke_width=stroke_width,
     stroke_color=stroke_color,
     background_color=bg_color,
-    height=200,
-    width=200,
+    height=400,
+    width=400,
     key="canvas",
 )
